6c7ca2d6b68cff6ddd287b4376927f152653a4d3
Natalie <nataliembatha@gmail.com>
2023-04-13 16:48:17 +0200

    Styling the tabs

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import City from './src/screens/City';
 import CurrentWeather from './src/screens/CurrentWeather';
 import UpcomingWeather from './src/screens/UpcomingWeather';
@@ -11,10 +12,51 @@ const Tab = createBottomTabNavigator()
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name={'Current'} component={CurrentWeather} />
-        <Tab.Screen name={'Upcoming'} component={UpcomingWeather} />
-        <Tab.Screen name={'City'} component={City} />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: 'magenta',
+          tabBarInactiveTintColor: 'gray'
+        }}
+      >
+        <Tab.Screen 
+          name={'Current'} 
+          component={CurrentWeather}
+          options={{ 
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name={"today"} 
+                size={25} 
+                color={ focused ? '#dc143c' : '#000000' } 
+              />
+            )
+          }}
+        />
+        <Tab.Screen 
+          name={'Upcoming'} 
+          component={UpcomingWeather}
+          options={{ 
+            tabBarIcon: ({ focused }) => (
+              <Ionicons 
+                name={"timer"} 
+                size={25} 
+                color={ focused ? '#dc143c' : '#000000' } 
+              />
+            ) 
+          }} 
+        />
+        <Tab.Screen 
+          name={'City'} 
+          component={City}
+          options={{ 
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name={"ios-location-sharp"} 
+                size={25} 
+                color={ focused ? '#dc143c' : '#000000' } 
+              />
+            ) 
+          }} 
+        />
       </Tab.Navigator>
     </NavigationContainer>
   )
