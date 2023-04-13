535216de357ba3b09e1f0832720fae66cf63e172
Natalie <nataliembatha@gmail.com>
2023-04-13 15:30:02 +0200

    Wrapped the whole App with the NavigationContainer

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { StyleSheet, View  } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import City from './src/screens/City';
 import CurrentWeather from './src/screens/CurrentWeather';
 import UpcomingWeather from './src/screens/UpcomingWeather';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      {/* <CurrentWeather /> */}
-      {/* <UpcomingWeather /> */}
-      <City />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        {/* <CurrentWeather /> */}
+        {/* <UpcomingWeather /> */}
+        <City />
+      </View>
+    </NavigationContainer>
   )
 }
 
